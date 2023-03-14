fc3e0c3501aa8f9f01511a899818de8084306378
LuisK <luiskfibe2@gmail.com>
2023-03-14 10:42:09 -0500

    Se ha terminado el punto 1 que corresponde al laboratorio 1.

--- app/src/main/java/com/lcarlosfb/p0/presentation/notas/NotasActivity.kt
+++ app/src/main/java/com/lcarlosfb/p0/presentation/notas/NotasActivity.kt
@@ -23,27 +23,30 @@ class NotasActivity : AppCompatActivity() {
 			val nota3 : String = notasBinding.edtSegundoAvance.text.toString()
 			val nota4 : String = notasBinding.edtProyectoFinal.text.toString()
 
-			if (nota1.isEmpty() || nota2.isEmpty() || nota3.isEmpty() || nota4.isEmpty())
+			if (nota1.isEmpty() || nota2.isEmpty() || nota3.isEmpty() || nota4.isEmpty()) {
 				Toast.makeText(applicationContext, "Hay campos vacios", Toast.LENGTH_SHORT).show()
 
-			else{
+			}else{
 				val suma = (((nota1.toFloat() * 0.6) + (nota2.toFloat() * 0.07) + (nota3.toFloat() * 0.08) +
 						(nota4.toFloat() * 0.25))*1000).roundToInt().toDouble()/1000
-
 				notasBinding.tvResult.setText(suma.toString())
 			}
 
 		notasBinding.btnReset.setOnClickListener {
-
-			notasBinding.tvResult.setText("0")
-			notasBinding.edtPrimerAvance.setText("")
-			notasBinding.edtSegundoAvance.setText("")
-			notasBinding.edtProyectoFinal.setText("")
-			notasBinding.edtLab.setText("")
-
+			limpiar()
 		}
 
 		}
 
 	}
+
+	private fun limpiar() {
+		notasBinding.tvResult.setText("0")
+		notasBinding.edtPrimerAvance.setText("")
+		notasBinding.edtSegundoAvance.setText("")
+		notasBinding.edtProyectoFinal.setText("")
+		notasBinding.edtLab.setText("")
+	}
 }
+
+//Punto 1 de la version 1.0 terminado
