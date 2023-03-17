04359c32ace216ef13fb0b7fc45a60a81314ed33
LuisK <luiskfibe2@gmail.com>
2023-03-17 15:36:39 -0500

    Se ha terminado el punto 1 que corresponde al laboratorio 2, ViewModel y livedata implementados.

--- app/src/main/java/com/lcarlosfb/p0/presentation/notas/NotasActivity.kt
+++ app/src/main/java/com/lcarlosfb/p0/presentation/notas/NotasActivity.kt
@@ -3,40 +3,49 @@ package com.lcarlosfb.p0.presentation.notas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.lcarlosfb.p0.databinding.ActivityNotasBinding
-import kotlin.math.roundToInt
 
 class NotasActivity : AppCompatActivity() {
 
-	private lateinit var notasBinding: ActivityNotasBinding
+	private lateinit var notasBinding : ActivityNotasBinding
+	private lateinit var notasViewModel : NotasViewModel
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		notasBinding = ActivityNotasBinding.inflate(layoutInflater)
+		notasViewModel = ViewModelProvider(this)[NotasViewModel::class.java]
 		val view = notasBinding.root
 		setContentView(view)
 
+		val validarCamposObserver = Observer<String>{ nError ->
+			Toast.makeText(applicationContext, nError, Toast.LENGTH_SHORT).show()
+		}
+		notasViewModel.nError.observe(this, validarCamposObserver)
+
+		val sumTotalObserver = Observer <Float> {total ->
+			notasBinding.tvResult.setText(total.toString())
+		}
+		notasViewModel.total.observe(this, sumTotalObserver)
+
+
 		notasBinding.btnCalcular.setOnClickListener {
-
-			val nota1 : String = notasBinding.edtLab.text.toString()
-			val nota2 : String = notasBinding.edtPrimerAvance.text.toString()
-			val nota3 : String = notasBinding.edtSegundoAvance.text.toString()
-			val nota4 : String = notasBinding.edtProyectoFinal.text.toString()
-
-			if (nota1.isEmpty() || nota2.isEmpty() || nota3.isEmpty() || nota4.isEmpty()) {
-				Toast.makeText(applicationContext, "Hay campos vacios", Toast.LENGTH_SHORT).show()
-
-			}else{
-				val suma = (((nota1.toFloat() * 0.6) + (nota2.toFloat() * 0.07) + (nota3.toFloat() * 0.08) +
-						(nota4.toFloat() * 0.25))*1000).roundToInt().toDouble()/1000
-				notasBinding.tvResult.setText(suma.toString())
-			}
+			val (nota1, nota2, nota3, nota4) = leerDatos()
+			notasViewModel.calcular(nota1,nota2,nota3,nota4)
 		}
 
 		notasBinding.btnReset.setOnClickListener {
 			limpiar()
 		}
+	}
 
+	private fun leerDatos(): Array<String> {
+		val nota1 : String = notasBinding.edtLab.text.toString()
+		val nota2 : String = notasBinding.edtPrimerAvance.text.toString()
+		val nota3 : String = notasBinding.edtSegundoAvance.text.toString()
+		val nota4 : String = notasBinding.edtProyectoFinal.text.toString()
+		return arrayOf(nota1,nota2,nota3,nota4)
 	}
 
 	private fun limpiar() {
@@ -49,3 +58,5 @@ class NotasActivity : AppCompatActivity() {
 }
 
 //Punto 1 de la version 1.0 terminado
+
+//Punto 1 de la version 2.0 terminado
